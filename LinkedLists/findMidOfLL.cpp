bb028b9a6f4d13bc9d43abe070057f25919e7ac4
GauravCGS <52190746+GauravCGS@users.noreply.github.com>
2024-03-21 12:06:56 +0530

    Update findMidOfLL.cpp
    Used better code, while condition changed

--- LinkedLists/findMidOfLL.cpp
+++ LinkedLists/findMidOfLL.cpp
@@ -13,9 +13,8 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast->next != NULL){
-            if(fast->next->next != NULL) fast = fast->next->next;
-            else fast = fast->next;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
             slow = slow->next;
         }
         return slow;
