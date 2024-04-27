0f27e325f2be9fdbdaf2485439584264b198b768
GauravCGS <52190746+GauravCGS@users.noreply.github.com>
2024-04-27 13:35:26 +0530

    Update sumTreeOrNot.cpp

--- Trees/sumTreeOrNot.cpp
+++ Trees/sumTreeOrNot.cpp
@@ -28,3 +28,33 @@ class Solution
          return false;
     }
 };
+
+// n soln
+
+class Solution
+{
+    public:
+    
+    int findSum(Node* node, bool &f){
+        if(node == NULL){
+            return 0;
+        }
+        if(node->left == NULL && node->right == NULL){
+            return node->data;
+        }
+        int sumL = findSum(node->left,f);
+        int sumR = findSum(node->right,f);
+        if(node->data != sumL + sumR) f = false;
+        return node->data + sumL +sumR;
+    }
+    
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+         Node* node = root;
+         bool f = true;
+         int sum = 0;
+         findSum(node,f);
+         return f;
+    }
+};
