de7194632eb2bdb69f0910d57fae77fe447c9ea8
GauravCGS <52190746+GauravCGS@users.noreply.github.com>
2024-05-16 23:25:58 +0530

    Update topViewBinaryTree.cpp
    approach 2 here we traverse through the tree in level order fashion and maintaining a pair<node, horizontal distance> and we keep storing nodes hd as key and data as value in a map.
    If the hd is already available in the map do nothing else push data in the map with hd as key...
    So the idea is that the boundary nodes will be stored and the inner nodes will always have overlapping hds

--- Trees/topViewBinaryTree.cpp
+++ Trees/topViewBinaryTree.cpp
@@ -47,3 +47,47 @@ class Solution
     }
 
 };
+
+
+//approach 2
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    void solve(Node *node,int x, map<int, int> &m){
+        queue<pair<Node*, int>>q;
+        q.push({node,x});
+        int min = 0, max = 0;
+        while(!q.empty()){
+            Node* n = q.front().first;
+            int hd = q.front().second;
+            if(!m[hd]){
+                m[hd] = n->data;
+            }
+            if(n->left){
+                q.push({n->left,hd-1});
+            }
+            if(n->right){
+                q.push({n->right,hd+1});
+            }
+            q.pop();
+        }
+        // cout<<min<<" : "<<max;
+    }
+    
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+        vector<int>res;
+        map<int, int> m;
+        // m.insert({0,root->data});
+        // res.push_back(root->data);
+        solve(root,0,m);
+        for(auto i : m){
+            res.push_back(i.second);
+        }
+        return res;
+    }
+
+};
