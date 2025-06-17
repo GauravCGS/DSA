569d0747ccc48a2eabcf9504b9111e20a1f2e2ff
GauravCGS <52190746+GauravCGS@users.noreply.github.com>
2025-06-17 19:20:52 +0530

    Update RemoveDuplicatesFromArray.cpp
    Approach 2 with 2 pointers

--- Arrays/RemoveDuplicatesFromArray.cpp
+++ Arrays/RemoveDuplicatesFromArray.cpp
@@ -9,3 +9,15 @@ int removeDuplicates(vector<int> &arr, int n) {
 	}
 	return arr.size();
 }
+
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        for(int j = 1; j < n; j++){
+           if(nums[i] != nums[j]){
+            i++;
+            nums[i] = nums[j];
+           }  
+        }
+        return i+1;
+    }
